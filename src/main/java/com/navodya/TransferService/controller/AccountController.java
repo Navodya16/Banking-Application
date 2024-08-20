6b6cb126e6736d90582efe18885fa9fb52e12475
Navodya16 <prabuddhi.rath2@gmail.com>
2024-08-21 00:07:55 +0530

    connected in memory h2 datbase

--- src/main/java/com/navodya/TransferService/controller/AccountController.java
+++ src/main/java/com/navodya/TransferService/controller/AccountController.java
@@ -2,8 +2,8 @@ package com.navodya.TransferService.controller;
 
 import java.util.List;
 
-import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
